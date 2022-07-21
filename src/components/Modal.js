a71c989845abb68873c4d9e4a2853fa01a6c82de
skipeverylunch <skipeverylunch@outlook.jp>
2022-07-22 08:04:06 +0900

    style:changed layout of Modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,8 +2,8 @@ import React from 'react';
 const Modal = ({ message }) => {
   return (
     <div className="fixed z-1 grid place-items-center w-full h-full transition-opacity bg-gray-900 bg-opacity-40">
-      <div className="bg-white bg-opacity-40 w-full h-1/5 grid place-items-center">
-        <div className="text-4xl">{message}</div>
+      <div className="bg-white w-full h-1/5 grid place-items-center">
+        <div className="text-6xl">{message}</div>
       </div>
     </div>
   );

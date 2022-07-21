0bc5fee44004572ccf50e298c6c3481922f71f50
skipeverylunch <skipeverylunch@outlook.jp>
2022-07-22 08:00:30 +0900

    refactor:tcreate gameState and setGameState

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import CellData from '../classes/CellData';
 import Cell from './Cell';
+import Modal from './Modal';
 import {
   compareArrays,
   randomMultiFrom2DArray,
@@ -8,6 +9,8 @@ import {
 } from '../helpers';
 const Board = () => {
   const [cells, setCells] = useState([]);
+  const [gameState, setGameState] = useState('onProgress');
+  const [modalMessage, setModalMessage] = useState('');
   const size = 9;
   const numOfMines = 10;
   //TODO:useCallbackでキャッシュ化したい
@@ -52,18 +55,10 @@ const Board = () => {
         return prev + sum;
       }, 0);
       if (!isBombed && flipedCells >= size * size - numOfMines) {
-        setTimeout(() => {
-          alert('congrats! you clear');
-          initializeGame();
-        }, 250);
-        //TODO:モーダルで出したい
+        setGameState('won');
       }
       if (isBombed) {
-        setTimeout(() => {
-          alert('you dead');
-          //TODO:モーダルで出したい
-          initializeGame();
-        }, 250);
+        setGameState('lose');
       }
     },
     [cells]
@@ -88,6 +83,7 @@ const Board = () => {
     [cells]
   );
   const initializeGame = () => {
+    setGameState('onProgress');
     //initiate game
     //create empty cells
     const newCells = [];
@@ -122,10 +118,30 @@ const Board = () => {
     setCells(newCells);
   };
   useEffect(() => {
-    initializeGame();
-  }, []);
+    if (gameState === 'lose') {
+      setTimeout(() => {
+        setModalMessage('you dead');
+        setTimeout(() => {
+          setModalMessage('');
+          initializeGame();
+        }, 1500);
+      }, 250);
+    } else if (gameState === 'won') {
+      setTimeout(() => {
+        setModalMessage('clear.Congrats!');
+        setTimeout(() => {
+          setModalMessage('');
+          initializeGame();
+        }, 1500);
+      }, 250);
+    } else {
+      initializeGame();
+    }
+  }, [gameState]);
   return (
     <div className="h-screen grid place-items-center">
+      {modalMessage.length > 0 ? <Modal message={modalMessage} /> : null}
+
       <div className="flex flex-col gap-1">
         {cells.length
           ? cells.map((el) => {
